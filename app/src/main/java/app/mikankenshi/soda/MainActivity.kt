65bc3c852501046ee49c4a6b4c311e00e2d83c6b
redsharei <syarei@live.jp>
2019-11-30 22:50:52 +0900

    time start stop 0.01sec no

--- app/src/main/java/app/mikankenshi/soda/MainActivity.kt
+++ app/src/main/java/app/mikankenshi/soda/MainActivity.kt
@@ -12,10 +12,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
     val handler = Handler()
     var num = 0
-//    var numTime = 0
+    //    var numTime = 0
     var time = 0
     var judge = false
     var onetime = true
+
+
+    val runnable = object : Runnable {
+        // メッセージ受信が有った時かな?
+        override fun run() {
+            time++                      // 秒カウンタ+1
+            timeToText(time)?.let {
+                // timeToText()で表示データを作り
+                timeView.text = it            // timeText.textへ代入(表示)
+            }
+            handler.postDelayed(this, 10)  // 10ｍｓ後に自分にpost
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,51 +39,56 @@ class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
         val sd = ShakeDetector(this)
         sd.start(sensorManager)
 
-     //   bar.setMax(100)
-     //   bar.setProgress(80)
 
 
         restert.setOnClickListener {
+            handler.post(runnable)
+        }
+/*
+        stop.setOnClickListener {
+            handler.removeCallbacks(runnable)
+        }
+*//*
+        restert.setOnClickListener {
             num = 0
             progress.setImageLevel(num)
+            handler.removeCallbacks(runnable)      // キューキャンセル
+            time = 0                          // 秒カウンタークリア
+            timeToText()?.let {                  // timeToText()で表示データを作り
+                timeView.text = it                // timeText.textに表示
+            }
+            onetime = true
         }
-
+*/
         stop.setOnClickListener {
             num = num
             progress.setImageLevel(num)
-            when(judge){
-                false -> {stop.text = "START"
-                    judge = true}
-                else -> {stop.text = "STOP"
-                    judge = false}
+            when (judge) {
+                false -> {
+                    stop.text = "START"
+                    judge = true
+                }
+                else -> {
+                    stop.text = "STOP"
+                    judge = false
+                }
             }
         }
+
         progress.setImageResource(R.drawable.progress_background)
         progress.setImageLevel(0)
-       // progress.getDrawable().setLevel(500)
-
     }
-
 
 
     override fun hearShake() {
         // やりたい処理を書く
-        if(onetime) {
-            val runnable = object : Runnable {
-                // メッセージ受信が有った時かな?
-                override fun run() {
-                    time++                      // 秒カウンタ+1
-                    timeToText(time)?.let {
-                        // timeToText()で表示データを作り
-                        timeView.text = it            // timeText.textへ代入(表示)
-                    }
-                    handler.postDelayed(this, 1000)  // 1000ｍｓ後に自分にpost
-                }
-            }
+
+        if (onetime) {
             handler.post(runnable)
-            // if(num==10000)  handler.removeCallbacks(runnable)
-        onetime = false
+            onetime = false
         }
+
+        if(num == 5000) handler.removeCallbacks(runnable)
 
 
         num += 1000
@@ -77,21 +96,23 @@ class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
         Toast.makeText(this@MainActivity, Integer.toString(num), Toast.LENGTH_SHORT).show()
 
     }
-/*
-    fun startTimer(): String{
 
-    }
-*/
+
     private fun timeToText(time: Int = 0): String? {
         return if (time < 0) {
             null                                    // 時刻が0未満の場合 null
         } else if (time == 0) {
             "00:00:00"                            // ０なら
         } else {
+            /*
             val h = time / 3600
             val m = time % 3600 / 60
             val s = time % 60
- //           val ss = time
+            */
+            val h = time /3600
+            val m = time % 3600 / 60
+            val s = time % 100
+
             "%1$02d:%2$02d:%3$02d".format(h, m, s)  // 表示に整形
         }
     }
@@ -102,3 +123,4 @@ class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
 //0.1秒
 //stop
 //ランキング
+//level（ゲージ量）
